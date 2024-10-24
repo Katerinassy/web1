9e07065edd59eca18a9172fca5f11eee85a008f8
source_dev <prifaryt@gmail.com>
2024-10-24 14:51:30 +0300

    продолжение

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import logo from "./images/Logo.svg";
 import search from "./images/SearchIcon.svg";
 import cart from "./images/CartIcon.svg";
+import aeroww from "./images/Aerrow.png";
+import 
 
 function App() {
   return (
@@ -39,7 +41,19 @@ function App() {
           <p>cart(0)</p>
         </div>
       </div>
+      <div className="block_one">
+        <div className="header_block2">
+        <p className="naturalfood">100% Natural Food</p>
+        <p className="choose">Choose the best <br /> healthier way <br />  of life</p>
+        <button className="button_block_one">Explore Now
+          <img src={aeroww} alt=""  />
+        </button>
+        </div>
+      
+    
+      </div>
     </div>
+  
   );
 }
 
